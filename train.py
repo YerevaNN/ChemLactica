21a023ac924d4cddfb9ef4a3e24822ddc49bef54
Philipp Guevorguian <pguevorg@uwo.ca>
2023-06-28 01:17:38 +0400

    add perplexity evaluation

--- train.py
+++ train.py
@@ -3,18 +3,24 @@ from transformers import AutoTokenizer
 from transformers import AutoModelForCausalLM
 from transformers import Trainer, TrainingArguments
 from datasets import load_dataset
+from eval_metrics import compute_metrics
 
 model_checkpoint = "facebook/galactica-125m"
 
 model = AutoModelForCausalLM.from_pretrained(model_checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
-def process_str(str):
-    str['text'] = str['text'].replace('\\','')
-    return str 
 
-dataset = load_dataset("text", data_files='./523129_start.jsonl', split='train', streaming=True)
+def process_str(str):
+    str["text"] = str["text"].replace("\\", "")
+    return str
+
+
+dataset = load_dataset(
+    "text", data_files="./523129_start.jsonl", split="train", streaming=True
+)
 dataset = dataset.map(process_str)
+
 
 def tokenize_function(examples):
     return tokenizer(examples["text"])
@@ -25,10 +31,10 @@ tokenized_datasets = dataset.map(tokenize_function, batched=True)
 model_name = model_checkpoint.split("/")[-1]
 training_args = TrainingArguments(
     f"{model_name}-finetuned-pubchem",
-    evaluation_strategy = "epoch",
+    evaluation_strategy="epoch",
     learning_rate=2e-5,
     weight_decay=0.01,
-    max_steps=2
+    max_steps=2,
 )
 
 trainer = Trainer(
@@ -36,6 +42,7 @@ trainer = Trainer(
     args=training_args,
     train_dataset=dataset,
     # eval_dataset=lm_datasets["validation"],
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
