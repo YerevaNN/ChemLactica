8c37be1d0ba4cdcbefaba0e61a256426e7e2e3a3
tigranthegreat <tigran.fahradyan01@gmail.com>
2023-06-28 14:39:12 +0400

    use AimCallback

--- train.py
+++ train.py
@@ -4,21 +4,24 @@ from transformers import AutoModelForCausalLM
 from transformers import Trainer, TrainingArguments
 from datasets import load_dataset
 from eval_metrics import compute_metrics
-import aim_utils
+from aim.hugging_face import AimCallback
+
 
 def process_str(str):
     str["text"] = str["text"].replace("\\", "")
     return str
 
+
 def tokenize_function(examples):
     return tokenizer(examples["text"])
+
 
 def group_texts(examples):
     # Concatenate all texts.
     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
     total_length = len(concatenated_examples[list(examples.keys())[0]])
     # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
+    # customize this part to your needs.
     total_length = (total_length // block_size) * block_size
     # Split by chunks of max_len.
     result = {
@@ -28,25 +31,31 @@ def group_texts(examples):
     result["labels"] = result["input_ids"].copy()
     return result
 
+
 def tokenize_function(examples):
     return tokenizer(examples["text"])
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     model_checkpoint = "facebook/galactica-125m"
     block_size = 128
 
     model = AutoModelForCausalLM.from_pretrained(model_checkpoint)
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
-    dataset = load_dataset("text", data_files={"train": './train.jsonl', "validation": './evaluation.jsonl'}, streaming=True)
+    dataset = load_dataset(
+        "text",
+        data_files={"train": "./train.jsonl", "validation": "./evaluation.jsonl"},
+        streaming=True,
+    )
     dataset = dataset.map(process_str)
-    tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns=["text"])
-    lm_datasets = tokenized_datasets.map(
-        group_texts,
-        batched=True,
-        batch_size=1000
-    ) 
-    
+    tokenized_datasets = dataset.map(
+        tokenize_function, batched=True, remove_columns=["text"]
+    )
+    lm_datasets = tokenized_datasets.map(group_texts, batched=True, batch_size=1000)
+
+    aim_callback = AimCallback(repo=".", experiment="your_experiment_name")
+
     training_args = TrainingArguments(
         output_dir=f"{model_checkpoint.split('/')[-1]}-finetuned-pubchem",
         evaluation_strategy="epoch",
@@ -59,9 +68,9 @@ if __name__ == "__main__":
         model=model,
         args=training_args,
         compute_metrics=compute_metrics,
-        train_dataset=lm_datasets['train'],
+        train_dataset=lm_datasets["train"],
         eval_dataset=lm_datasets["validation"],
-        callbacks=[aim_utils.AimTrackerCallback],
+        callbacks=[aim_callback],
     )
 
     trainer.train()
