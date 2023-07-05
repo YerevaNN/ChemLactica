4690c9fba1191f3f931c7d883f0e80ea1b7a1e3e
tigranthegreat <tigran.fahradyan01@gmail.com>
2023-07-05 13:48:15 +0400

    fix the directory names

--- precommit_test.py
+++ precommit_test.py
@@ -6,8 +6,8 @@ class TestNetworkTraining(unittest.TestCase):
     def test_small_opt_model(self):
         executed_prog = subprocess.run(
             "python3 train.py --model_type 125m --max_steps 1 \
-                --training_data_dir small_data/train \
-                --valid_data_dir small_data/valid \
+                --training_data_dir .small_data/train \
+                --valid_data_dir .small_data/valid \
                 --load_small_opt true",
             shell=True,
             # stdout=subprocess.DEVNULL,
