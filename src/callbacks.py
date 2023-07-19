ccdef8f64c48efb3da06ab9932ad7f14ce398d94
Philipp Guevorguian <pguevorg@uwo.ca>
2023-07-19 23:40:43 +0400

    fix norm tracking

--- src/callbacks.py
+++ src/callbacks.py
@@ -18,7 +18,8 @@ class CustomAimCallback(AimCallback):
         self._checkpoints_dict_name = checkpoints_dict_name
         self.model = model
         self.setup()
-        self.embedding_norm = 0
+        self.embedding_norm_1 = 0
+        self.embedding_norm_2 = 0
         self.activations_norm = 0
         self._run[self._checkpoints_dict_name] = {}
         self.blocksize = blocksize
@@ -36,33 +37,28 @@ class CustomAimCallback(AimCallback):
             )
         self._run[self._checkpoints_dict_name] = checkpoints_dict
 
-    def on_step_end(self, args, state, control=None, **kwargs):
-        self.embedding_norm_1 = torch.linalg.norm(
-            self.model.get_input_embeddings().weight, p=1
-        )
-        self.embedding_norm_2 = torch.linalg.norm(
-            self.model.get_input_embeddings().weight, p=2
-        )
+    def on_train_begin(self, args, state, control, **kwargs):
+        super().on_train_begin(args, state, control, **kwargs)
+        self.start_time = time.time()
 
+    def on_step_end(self, args, state, control, **kwargs):
+        # Get batch size (first dimension of inputs)
+        self.embedding_norm_1 = torch.linalg.norm(
+            self.model.get_input_embeddings().weight, ord=1
+        )
         self.embedding_norm_2 = torch.linalg.norm(
-            self.model.get_input_embeddings().weigh
+            self.model.get_input_embeddings().weight, ord=2
         )
 
         self.experiment.track(self.embedding_norm_1, name="embedding l1 norm")
         self.experiment.track(self.embedding_norm_2, name="embedding l2 norm")
-    def on_train_begin(self, args, state, control, **kwargs):
-        super().on_train_begin(args, state, control, **kwargs)
-        self.start_time = time.time()
-    def on_step_end(self, args, state, control, model=None, **kwargs):
-        # Get batch size (first dimension of inputs)
 
-        batch_size = args.per_device_train_batch_size # kwargs['model_inputs']['input_ids'].shape[0]
+        batch_size = (
+            args.per_device_train_batch_size
+        )  # kwargs['model_inputs']['input_ids'].shape[0]
         # Calculate tokens in batch
-        num_words = batch_size * self.blocksize  #args.blocksize
+        num_words = batch_size * self.blocksize  # args.blocksize
         # Calculate time taken for this step
-        elapsed_time = time.time() - self.start_time        # Calculate words per second
+        elapsed_time = time.time() - self.start_time  # Calculate words per second
         words_per_second = num_words / elapsed_time
-        self.experiment.track(words_per_second, name='words_per_second')
-
-
-    
+        self.experiment.track(words_per_second, name="words_per_second")
