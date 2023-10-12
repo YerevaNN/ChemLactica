bc8a8b6aa1eabd6e33b4ebaffec04157386eb2a6
Philipp Guevorguian <pguevorg@uwo.ca>
2023-10-12 21:32:35 +0400

    fix incorrect lengths for assay data

--- new_docmaker.py
+++ new_docmaker.py
@@ -21,9 +21,9 @@ def iter_get_num_tokens(list_ass):
 
 def add_var_str(var_object):
     document = ""
-    document += "[VARNAME" + str(var_object["name"]) + "]"
-    document += "[VARDESC" + str(var_object["description"]) + "]"
-    document += "[VARVALUE" + str(var_object["value"]) + "]"
+    document += "[VARNAME " + str(var_object["name"]) + "]"
+    document += "[VARDESC " + str(var_object["description"]) + "]"
+    document += "[VARVALUE " + str(var_object["value"]) + "]"
     return document
 
 
@@ -34,8 +34,7 @@ def remove_from_all_values(dict_type, num_to_remove):
 
 
 def evenly_remove_elements_from_lists(lists, total_elements_to_remove):
-    num_lists = len(lists)
-    print("number of lists", num_lists)
+    # num_lists = len(lists)
     # extra_elements = total_elements_to_remove % num_lists
 
     # for i in range(num_lists):
@@ -51,6 +50,17 @@ def remove_big_assays(assays):
     return assays
 
 
+def process_assays(assays):
+    assays = json_data["assays"]
+
+    sorted_assays = sorted(assays, key=lambda x: len(x["description"]), reverse=False)
+
+    print("number of assays before removing big ones", len(sorted_assays))
+    sorted_assays = remove_big_assays(sorted_assays)
+    print("number of assays before removing big ones", len(sorted_assays))
+    return sorted_assays
+
+
 def group_the_lists(list_of_BE_lists):
     input_ids = []
     token_type_ids = []
@@ -58,7 +68,6 @@ def group_the_lists(list_of_BE_lists):
     number_of_lists = len(list_of_BE_lists)
     for index in range(number_of_lists):
         for element in list_of_BE_lists[index]:
-            # print("element",type(element))
             input_ids.extend(element["input_ids"])
             token_type_ids.extend(element["token_type_ids"])
             attention_mask.extend(element["attention_mask"])
@@ -75,39 +84,31 @@ def group_the_lists(list_of_BE_lists):
 
 
 context_length = CONTEXT_LENGTH
+need_new_assay = True
+assay_num = 0
 
 
 with open(jsonl_file_path, "r") as jsonl_file:
     for index, line in enumerate(jsonl_file):
-        # Parse the JSON object from the current line
+        # Parse the JSON compound from the current line
         json_data = json.loads(json.loads(line))
-        documents = []
-
-        assays = json_data["assays"]
-        sorted_assays = sorted(
-            assays, key=lambda x: len(x["description"]), reverse=False
-        )
-
-        print("number of assays before removing big ones", len(sorted_assays))
-        sorted_assays = remove_big_assays(sorted_assays)
-        print("number of assays before removing big ones", len(sorted_assays))
+        sorted_assays = process_assays(json_data["assays"])
 
         need_new_assay = True
         assay_num = 0
+        documents = []
 
+        # Loop until the compound has no more associated assays
         while sorted_assays:
             doc_len_dic = {"desc": 0, "name": 0, "var": 0}
-            doc_len = 0
-            curr_doc_ass_names = []
-            curr_doc_ass_descs = []
-            curr_doc_ass_vars = []
+            doc_dict = {"names": [], "descriptions": [], "variables": []}
             tok_ass_vars = []
 
-            while doc_len < context_length:
-                print("descsuma", iter_get_num_tokens(curr_doc_ass_descs))
-                print(doc_len_dic)
+            # loop until we fill full context
+            while (
+                doc_len_dic["desc"] + doc_len_dic["name"] + doc_len_dic["var"]
+            ) < context_length:
                 if need_new_assay:
-                    tok_ass_vars = []
                     try:
                         assay = sorted_assays.pop()
                         variables = assay["variables"]
@@ -115,108 +116,57 @@ with open(jsonl_file_path, "r") as jsonl_file:
                     except Exception as e:
                         print(e)
                         break
-                    tok_ass_name = tokenizer(assay["name"])
-                    tok_ass_desc = tokenizer(assay["description"])
-                    tok_name_len = get_num_tokens(tok_ass_name)
-                    tok_desc_len = get_num_tokens(tok_ass_desc)
-                if doc_len == 0 or need_new_assay:
-                    curr_doc_ass_names.append(tok_ass_name)
-                    curr_doc_ass_descs.append(tok_ass_desc)
-                    doc_len += tok_name_len + tok_desc_len
+                    tok_ass_name = tokenizer("[ASSNAME " + assay["name"] + "]")
+                    tok_ass_desc = tokenizer("[ASSDESC " + assay["description"] + "]")
+                if doc_len_dic["name"] == 0 or need_new_assay:
+                    doc_dict["names"].append(tok_ass_name)
+                    doc_dict["descriptions"].append(tok_ass_desc)
                     doc_len_dic["name"] += get_num_tokens(tok_ass_name)
                     doc_len_dic["desc"] += get_num_tokens(tok_ass_desc)
-                    # print("doc_len after adding name + desc", doc_len_dic)
                     need_new_assay = False
                     continue
 
+                # if current assay has no more data
                 if not variables:
-                    # print("tok vars",type(tok_ass_vars))
-                    curr_doc_ass_vars.append(tok_ass_vars)
+                    doc_dict["variables"].append(tok_ass_vars)
                     tok_ass_vars = []
                     need_new_assay = True
-                    # continue
-                    pass
+                    continue
                 else:
-                    need_new_assay = False
-                    var = variables.pop()
-                    new_var = tokenizer(add_var_str(var))
-                    doc_len += get_num_tokens(new_var)
-                    doc_len_dic["var"] += get_num_tokens(new_var)
-                    # print("doc_len after adding var", doc_len_dic)
-                    # print("appended var",type(new_var))
-                    tok_ass_vars.append(new_var)
-            # print("curr doc vars",curr_doc_ass_vars)
-            print("desc sumb", iter_get_num_tokens(curr_doc_ass_descs))
-            print(doc_len_dic)
+                    var_tokens = tokenizer(add_var_str(variables.pop()))
+                    doc_len_dic["var"] += get_num_tokens(var_tokens)
+                    tok_ass_vars.append(var_tokens)
             if tok_ass_vars:
-                # print("it was not empty")
-                curr_doc_ass_vars.append(tok_ass_vars)
-            # print("document length:", doc_len)
-            difference = doc_len - context_length
+                doc_dict["variables"].append(tok_ass_vars)
+
+            # check how much to remove from description
+            difference = (
+                doc_len_dic["desc"] + doc_len_dic["name"] + doc_len_dic["var"]
+            ) - context_length
             if difference > 0:
-                sum = 0
-                for a in curr_doc_ass_descs:
-                    sum += get_num_tokens(a)
-                name_sum = 0
-                for b in curr_doc_ass_names:
-                    name_sum += get_num_tokens(b)
-                var_sum = 0
-                for li in curr_doc_ass_vars:
-                    for m in li:
-                        var_sum += get_num_tokens(m)
-                lein = len(curr_doc_ass_descs)
-                print(
-                    "description sum for document =",
-                    iter_get_num_tokens(curr_doc_ass_descs),
+                doc_dict["descriptions"] = evenly_remove_elements_from_lists(
+                    doc_dict["descriptions"], difference
                 )
 
-                curr_doc_ass_descs = evenly_remove_elements_from_lists(
-                    curr_doc_ass_descs, difference
-                )
-                sum = 0
-                for a in curr_doc_ass_descs:
-                    sum += get_num_tokens(a)
-
-                print("after desc", sum)
-            curr_doc = []
-            doc_len = 0
-            assert len(curr_doc_ass_names) == len(curr_doc_ass_descs)
-            # print("names", type(curr_doc_ass_names[0]))
-            # print("descs", type(curr_doc_ass_descs[0]))
-            # print("CRITICAL")
-            curr_doc_ass_vars = list(itertools.chain.from_iterable(curr_doc_ass_vars))
-            # print("vars", type(curr_doc_ass_vars[0]))
-            batch_encoding = group_the_lists(
-                [curr_doc_ass_names, curr_doc_ass_descs, curr_doc_ass_vars]
+            doc_dict["variables"] = list(
+                itertools.chain.from_iterable(doc_dict["variables"])
             )
-            print("batch encoding length", len(batch_encoding["input_ids"]))
-            # for i in range(len(curr_doc_ass_names)):
-            #     curr_doc.extend(curr_doc_ass_names[i])
-            #     curr_doc.extend(curr_doc_ass_descs[i])
-            #     curr_doc.extend(curr_doc_ass_vars[i])
 
+            batch_encoding = group_the_lists(
+                [doc_dict["names"], doc_dict["descriptions"], doc_dict["variables"]]
+            )
             if get_num_tokens(batch_encoding) == context_length:
                 documents.append(batch_encoding)
             else:
                 wrong_count += 1
 
-            curr_doc_ass_names = []
-            curr_doc_ass_descs = []
-            curr_doc_ass_vars = []
+            doc_dict["names"] = []
+            doc_dict["descriptions"] = []
+            doc_dict["variables"] = []
             tok_ass_vars = []
-
-            print("did a doc")
         break
-print(wrong_count)
-# for index,doc in enumerate(documents):
-#     print(f"index {index} length {len(doc)}")
-#     print(type(doc))
-# for doc in documents:
-#     print("------------------------------------------")
-#     decoded_string = tokenizer.decode(doc['input_ids'])
-#     print(decoded_string)
-#     encoded = tokenizer.encode(decoded_string)
 
-#     # Check the number of tokens
-#     num_tokens = len(encoded)
-#     print(num_tokens)
+print(tokenizer.decode(documents[2]["input_ids"]))
+print(len(documents[2]["input_ids"]))
+print(wrong_count)
+print(len(documents))
