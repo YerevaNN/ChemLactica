eb8c6f2c1af891b5b65618c77f25c50cd9927976
Philipp Guevorguian <pguevorg@uwo.ca>
2023-11-07 11:49:28 +0400

    increase learning rate to 5e-4

--- src/config/create_train_config.py
+++ src/config/create_train_config.py
@@ -15,3 +15,5 @@ for key in model_train_configs.keys():
 model_train_configs["1.3b"]["warmup_steps"] = 2000
 model_train_configs["1.3b"]["max_learning_rate"] = 1.0e-5
 model_train_configs["1.3b"]["global_gradient_norm"] = 0.1
+
+model_train_configs["125m"]["max_learning_rate"] = 5.0e-4
