93f39124511c0c3ee2ea2772a7eb818d9b81188d
Philipp Guevorguian <pguevorg@uwo.ca>
2023-11-12 23:39:27 +0400

    add mistral configs

--- src/config/create_train_config.py
+++ src/config/create_train_config.py
@@ -10,10 +10,20 @@ with open(full_path, "r") as f_:
     model_train_configs = yaml.full_load(f_)
 
 for key in model_train_configs.keys():
-    model_train_configs[key]["max_learning_rate"] *= 0.08
+    model_config = model_train_configs[key]
+    if "max_learning_rate" in model_config:
+        model_train_configs[key]["max_learning_rate"] *= 0.08
+
+
+model_train_configs["125m"]["max_learning_rate"] = 5.0e-4
+model_train_configs["125m"]["tokenizer_id"] = "src/tokenizer/ChemLacticaTokenizer"
+
+model_train_configs["mistral7b"][
+    "tokenizer_id"
+] = "src/tokenizer/Mistral-7B-v0.1Tokenizer"
+model_train_configs["mistral7b"]["max_learning_rate"] = 5.0e-4
+model_train_configs["mistral7b"]["warmup_steps"] = 2000
 
 model_train_configs["1.3b"]["warmup_steps"] = 2000
 model_train_configs["1.3b"]["max_learning_rate"] = 1.0e-5
 model_train_configs["1.3b"]["global_gradient_norm"] = 0.1
-
-model_train_configs["125m"]["max_learning_rate"] = 5.0e-4
