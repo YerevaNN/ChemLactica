e9d64e5ac94047bb2210f313f6136616c7afe471
Philipp Guevorguian <pguevorg@uwo.ca>
2023-11-29 19:54:14 +0400

    add llama2 model/training configuration

--- src/config/create_train_config.py
+++ src/config/create_train_config.py
@@ -21,9 +21,18 @@ model_train_configs["125m"]["tokenizer_id"] = "src/tokenizer/ChemLacticaTokenize
 model_train_configs["mistral7b"][
     "tokenizer_id"
 ] = "src/tokenizer/Mistral-7B-v0.1Tokenizer"
+
+
+model_train_configs["llama2"]["tokenizer_id"] = "src/tokenizer/llama2-tokenizer"
+
 model_train_configs["mistral7b"]["max_learning_rate"] = 5.0e-4
 model_train_configs["mistral7b"]["warmup_steps"] = 2000
 
 model_train_configs["1.3b"]["warmup_steps"] = 2000
 model_train_configs["1.3b"]["max_learning_rate"] = 1.0e-5
 model_train_configs["1.3b"]["global_gradient_norm"] = 0.1
+
+model_train_configs["llama2"]["tokenizer_id"] = "meta-llama/Llama-2-7b-hf"
+model_train_configs["llama2"]["warmup_steps"] = 500
+model_train_configs["llama2"]["max_learning_rate"] = 3.0e-5
+model_train_configs["llama2"]["global_gradient_norm"] = 0.1
