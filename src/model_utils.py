368c7e4eeaf7f1c82c7847511c83e29c54833081
Philipp Guevorguian <pguevorg@uwo.ca>
2023-11-29 19:55:37 +0400

    implement llama2 loading utility

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,11 +1,43 @@
-from transformers import OPTForCausalLM, OPTConfig
+from transformers import OPTForCausalLM, OPTConfig, LlamaForCausalLM
+import bitsandbytes as bnb
+from peft import get_peft_model, LoraConfig
 import torch
 from transformers import MistralForCausalLM
 from custom_modeling_opt import CustomOPTForCausalLM
+from transformers import BitsAndBytesConfig
+
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+
+
+def find_all_linear_names(model):
+    cls = (
+        bnb.nn.Linear4bit
+    )  # if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split(".")
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if "lm_head" in lora_module_names:  # needed for 16-bit
+        lora_module_names.remove("lm_head")
+    # if 'embed_tokens' in lora_module_names:
+    #     lora_module_names.remove('embed_tokens')
+    return list(lora_module_names)
 
 
 def load_model(
-    from_pretrained: str, use_flash_attn=False, dtype=None, train_config=None
+    from_pretrained: str,
+    use_flash_attn=True,
+    dtype=None,
+    train_config=None,
+    auth_token=None,
 ):
     if from_pretrained == "small_opt":
         return OPTForCausalLM(
@@ -29,4 +61,29 @@ def load_model(
             use_flash_attention_2=use_flash_attn,
             torch_dtype=torch.bfloat16,
         )
+    if "llama" in from_pretrained.lower():
+        print("it is llama")
+        model = LlamaForCausalLM.from_pretrained(
+            from_pretrained,
+            use_flash_attention_2=True,
+            token=auth_token,
+            quantization_config=quant_config,
+        )
+
+        model.config.use_cache = False
+        model.config.pretraining_tp = 1
+
+        modules = find_all_linear_names(model)
+        peft_config = LoraConfig(
+            lora_alpha=64,
+            lora_dropout=0.1,
+            r=64,
+            target_modules=modules,
+            inference_mode=False,
+            bias="none",
+            task_type="CAUSAL_LM",
+            modules_to_save=["embed_tokens", "lm_head"],
+        )
+        model.enable_input_require_grads()
+        model = get_peft_model(model, peft_config)
     return model
