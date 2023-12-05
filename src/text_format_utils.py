dab7b28582d03c0480ca5e666076383b72d1134a
tigranthegreat <tigran.fahradyan01@gmail.com>
2023-12-05 21:55:27 +0400

    add new tokenizer with special start and end tokens, round properties with floating point values to 2 decimal places

--- src/text_format_utils.py
+++ src/text_format_utils.py
@@ -2,7 +2,42 @@ import random
 import time
 from utils import get_tokenizer
 
-SPECIAL_TAGS = {"SMILES": {"start": "[START_SMILES]", "end": "[END_SMILES]"}}
+SPECIAL_TAGS = {
+    "SMILES": {"start": "[START_SMILES]", "end": "[END_SMILES]"},
+    "synonym": {"start": "[SYNONYM]", "end": "[/SYNONYM]"},
+    "RELATED": {"start": "[RELATED]", "end": "[/RELATED]"},
+    "similarity": {"start": "[SIMILAR]", "end": "[/SIMILAR]", "type": float},
+    "PROPERTY": {"start": "[PROPERTY]", "end": "[/PROPERTY]"},
+    "SAS": {"start": "[SAS]", "end": "[/SAS]", "type": float},
+    "WEIGHT": {"start": "[WEIGHT]", "end": "[/WEIGHT]", "type": float},
+    "TPSA": {"start": "[TPSA]", "end": "[/TPSA]", "type": float},
+    "CLOGP": {"start": "[CLOGP]", "end": "[/CLOGP]", "type": float},
+    "QED": {"start": "[QED]", "end": "[/QED]", "type": float},
+    "NUMHDONORS": {"start": "[NUMHDONORS]", "end": "[/NUMHDONORS]"},
+    "NUMHACCEPTORS": {"start": "[NUMHACCEPTORS]", "end": "[/NUMHACCEPTORS]"},
+    "NUMHETEROATOMS": {"start": "[NUMHETEROATOMS]", "end": "[/NUMHETEROATOMS]"},
+    "NUMROTATABLEBONDS": {"start": "[NUMROTATABLEBONDS]", "end": "[/NUMROTATABLEBONDS]"},
+    "NOCOUNT": {"start": "[NOCOUNT]", "end": "[/NOCOUNT]"},
+    "NHOHCOUNT": {"start": "[NHOHCOUNT]", "end": "[/NHOHCOUNT]"},
+    "RINGCOUNT": {"start": "[RINGCOUNT]", "end": "[/RINGCOUNT]"},
+    "HEAVYATOMCOUNT": {"start": "[HEAVYATOMCOUNT]", "end": "[/HEAVYATOMCOUNT]"},
+    "FRACTIONCSP3": {"start": "[FRACTIONCSP3]", "end": "[/FRACTIONCSP3]", "type": float},
+    "NUMAROMATICRINGS": {"start": "[NUMAROMATICRINGS]", "end": "[/NUMAROMATICRINGS]"},
+    "NUMSATURATEDRINGS": {"start": "[NUMSATURATEDRINGS]", "end": "[/NUMSATURATEDRINGS]"},
+    "NUMAROMATICHETEROCYCLES": {"start": "[NUMAROMATICHETEROCYCLES]", "end": "[/NUMAROMATICHETEROCYCLES]"},
+    "NUMAROMATICCARBOCYCLES": {"start": "[NUMAROMATICCARBOCYCLES]", "end": "[/NUMAROMATICCARBOCYCLES]"},
+    "NUMSATURATEDHETEROCYCLES": {"start": "[NUMSATURATEDHETEROCYCLES]", "end": "[/NUMSATURATEDHETEROCYCLES]"},
+    "NUMSATURATEDCARBOCYCLES": {"start": "[NUMSATURATEDCARBOCYCLES]", "end": "[/NUMSATURATEDCARBOCYCLES]"},
+    "NUMALIPHATICRINGS": {"start": "[NUMALIPHATICRINGS]", "end": "[/NUMALIPHATICRINGS]"},
+    "NUMALIPHATICHETEROCYCLES": {"start": "[NUMALIPHATICHETEROCYCLES]", "end": "[/NUMALIPHATICHETEROCYCLES]"},
+    "NUMALIPHATICCARBOCYCLES": {"start": "[NUMALIPHATICCARBOCYCLES]", "end": "[/NUMALIPHATICCARBOCYCLES]"},
+    "IUPAC": {"start": "[IUPAC]", "end": "[/IUPAC]"},
+    "VAR_NAME": {"start": "[VAR_NAME]", "end": "[/VAR_NAME]"},
+    "VAR_DESC": {"start": "[VAR_DESC]", "end": "[/VAR_DESC]"},
+    "VAR_VAL": {"start": "[VAR_VAL]", "end": "[/VAR_VAL]"},
+    "ASSAY_NAME": {"start": "[ASSAY_NAME]", "end": "[/ASSAY_NAME]"},
+    "ASSAY_DESC": {"start": "[ASSAY_DESC]", "end": "[/ASSAY_DESC]"}
+}
 
 
 def delete_empty_tags(compound_json):
@@ -26,23 +61,24 @@ def generate_formatted_string(compound_json):
 
 
 def format_key_value(key, value):
+    if key == "CID": return ""
     formatted_string = ""
     if key == "related":
         for pair in value:
-            formatted_string += f"[SIMILAR {pair['similarity']} {pair['SMILES']}]"
+            formatted_string += f"{SPECIAL_TAGS['similarity']['start']}{pair['SMILES']} {pair['similarity']}{SPECIAL_TAGS['similarity']['end']}"
     elif key == "experimental":
         for pair in value:
-            formatted_string += f"[{pair['PROPERTY_NAME']} {pair['PROPERTY_VALUE']}]"
+            formatted_string += f"[PROPERTY]{pair['PROPERTY_NAME']} {pair['PROPERTY_VALUE']}[/PROPERTY]"
     elif key == "synonyms":
         for val in value:
-            formatted_string += f"[SYNONYM {val['name']}]"
+            formatted_string += f"{SPECIAL_TAGS['synonym']['start']}{val['name']}{SPECIAL_TAGS['synonym']['end']}"
     else:
-        if key in SPECIAL_TAGS:
-            start = SPECIAL_TAGS[key]["start"]
-            end = SPECIAL_TAGS[key]["end"]
-            return f"{start}{value}{end}"
-        formatted_string = f"[{key.upper()} {value}]"
-        return formatted_string
+        if SPECIAL_TAGS[key].get('type') is float:
+            value = "{:.2f}".format(float(value))
+            assert len(value.split(".")[-1]) == 2
+        start = SPECIAL_TAGS[key]["start"]
+        end = SPECIAL_TAGS[key]["end"]
+        return f"{start}{value}{end}"
 
     return formatted_string
 
@@ -52,9 +88,9 @@ def main():
 
     string = """{\"synonyms\":[{\"name\":\"p-Phenylazo carbanilic acid, n-hexyl ester\"}],
                  \"related\":[{\"SMILES\":\"CCCCOC(=O)NC1=CC=C(C=C1)N\",
-                 \"similarity\":0.74}],\"experimental\":[],\"CID\":523129,
+                 \"similarity\":0.7}],\"experimental\":[{\"PROPERTY_NAME\":\"Kovats Retention Index\",\"PROPERTY_VALUE\":\"Semi-standard non-polar: 4303\"},{\"PROPERTY_NAME\":\"Kovats Retention Index\",\"PROPERTY_VALUE\":\"Semi-standard non-polar: 4303\"}],\"CID\":523129,
                  \"SMILES\":\"CCCCCCOC(=O)NC1=CC=C(C=C1)N=NC2=CC=CC=C2\",
-                 \"SAS\":2.09,\"WEIGHT\":325.18,\"TPSA\":63.05,\"CLOGP\":6.23,
+                 \"SAS\":2.0,\"WEIGHT\":325.1,\"TPSA\":63.06823,\"CLOGP\":6.231230123,
                  \"QED\":0.46,\"NUMHDONORS\":1,\"NUMHACCEPTORS\":4,
                  \"NUMHETEROATOMS\":5,\"NUMROTATABLEBONDS\":8,\"NOCOUNT\":5,
                  \"NHOHCOUNT\":1,\"RINGCOUNT\":2,\"HEAVYATOMCOUNT\":24,\"FRACTIONCSP3\":0.32,
@@ -68,7 +104,8 @@ def main():
     for i in range(100000):
         example = json.loads((string))
         example = delete_empty_tags(example)
-        generate_formatted_string((example))
+        print(generate_formatted_string((example)))
+        break
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(elapsed_time)
