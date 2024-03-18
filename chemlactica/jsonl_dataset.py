e9f409563a9a210a2b837144b0ed5851f20d35c3
Philipp Guevorguian <pguevorg@uwo.ca>
2024-03-18 09:23:28 +0400

    add self-distributing dataset for multi gpu training

--- chemlactica/jsonl_dataset.py
+++ chemlactica/jsonl_dataset.py
@@ -1,8 +1,12 @@
 from typing import List
-import torch
+
+# import torch
 
 # from io import StringIO
 import os
+from accelerate.state import PartialState
+
+distributed_state = PartialState()
 
 
 def generator_init_print(shared_jsonl_files, files):
@@ -23,51 +27,56 @@ def setup_generator(shared_jsonl_files, files):
     return file_states
 
 
-def get_batch(file, state, chunk_size):
-    with open(file) as f:
-        f.seek(state["position"])
-        batch = f.read(chunk_size)
-        if not batch:
-            raise StopIteration
+# def get_batch(file, state, chunk_size):
+#     with open(file) as f:
+#         f.seek(state["position"])
+#         batch = f.read(chunk_size)
+#         if not batch:
+#             raise StopIteration
 
-        batch += f.readline()
-        batch = batch.splitlines()
+#         batch += f.readline()
+#         batch = batch.splitlines()
 
-        # batch = [line.rstrip("\n") for line in batch]
-        state["position"] = f.tell()
-        batch_len = len(batch)
-        state["line_number"] += batch_len
-    return batch, batch_len, state
+#         # batch = [line.rstrip("\n") for line in batch]
+#         state["position"] = f.tell()
+#         batch_len = len(batch)
+#         state["line_number"] += batch_len
+#     return batch, batch_len, state
 
 
-def format_sample(sample, return_line_info, batch_len, file, state, i):
+def should_yield_on_current_rank(i, num_processes, process_index):
+    return i % num_processes == process_index
+
+
+def format_sample(line):
+    sample = line.strip()
     ret = {"text": sample}
-    if return_line_info:
-        ret["line_info"] = {
-            "file": file,
-            "line_number": state["line_number"] - batch_len + i,
-        }
     return ret
 
 
 def samples_generator(
     files: List[str], shared_jsonl_files, chunk_size=25000, return_line_info=False
 ):
-    if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
-        file_states = setup_generator(shared_jsonl_files, files)
+    file_states = setup_generator(shared_jsonl_files, files)
 
-        returned = True
-        while returned:
-            returned = False
-            for file, state in file_states.items():
-                try:
-                    batch, batch_len, state = get_batch(file, state, chunk_size)
-                except StopIteration:
-                    break
-                for i, sample in enumerate(batch, start=1):
-                    returned = True
-                    ret = format_sample(
-                        sample, return_line_info, batch_len, file, state, i
-                    )
-                    yield ret
-                shared_jsonl_files[file] = state
+    returned = True
+    while returned:
+        returned = False
+        for file, state in file_states.items():
+            with open(file) as f:
+                f.seek(state["position"])
+                line = f.readline()
+                counter = 0
+                while line:
+                    line = f.readline()
+                    if should_yield_on_current_rank(
+                        counter,
+                        distributed_state.num_processes,
+                        distributed_state.process_index,
+                    ):
+                        returned = True
+                        ret = format_sample(line)
+                        yield ret
+                    state["position"] = f.tell()
+                    shared_jsonl_files[file] = state
+                    counter = counter + 1
