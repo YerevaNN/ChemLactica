e82e20fd17fe37c73ca4202205d9f42075cc46ba
MenuaB <menooa.vold@gmail.com>
2024-05-02 20:26:57 +0400

    remove interleaved dataset test

--- tests/fsdp/test_model_training.py
+++ tests/fsdp/test_model_training.py
@@ -67,40 +67,40 @@ class TestModelTraining(unittest.TestCase):
         if out.returncode != 0:
             raise Exception(out.stderr.decode())
 
-    def test_model_train_interleaved(self):
-        # clean up
-        gc.collect()
-        torch.cuda.empty_cache()
+    # def test_model_train_interleaved(self):
+    #     # clean up
+    #     gc.collect()
+    #     torch.cuda.empty_cache()
 
-        command = create_train_command(
-            module="accelerate.commands.launch",
-            module_args={"config_file": self.config_file},
-            script=self.script,
-            script_args={
-                "train_type": "pretrain",
-                "from_pretrained": "facebook/galactica-125m",
-                "model_config": "galactica_125m_pretrain",
-                "training_data_dirs": f"{os.path.join(TD_PATH, 'comp_train')} {os.path.join(TD_PATH, 'assay_train')}",  # noqa
-                "dir_data_types": "computed assay",
-                "valid_data_dir": f"{os.path.join(TD_PATH, 'comp_valid')}",
-                "train_batch_size": 4,
-                "shuffle_buffer_size": 4,
-                "max_steps": 300,
-                "eval_steps": 2000,
-                "save_steps": 2000,
-                "dataloader_num_workers": 1,
-                "checkpoints_root_dir": os.path.join(TEST_DIR, "checkpoints"),
-                "experiment_name": "fsdp_model_train",
-                "gradient_accumulation_steps": 1,
-                "no_track": "",
-                "flash_attn": "",
-            },
-        )
+    #     command = create_train_command(
+    #         module="accelerate.commands.launch",
+    #         module_args={"config_file": self.config_file},
+    #         script=self.script,
+    #         script_args={
+    #             "train_type": "pretrain",
+    #             "from_pretrained": "facebook/galactica-125m",
+    #             "model_config": "galactica_125m_pretrain",
+    #             "training_data_dirs": f"{os.path.join(TD_PATH, 'comp_train')} {os.path.join(TD_PATH, 'assay_train')}",  # noqa
+    #             "dir_data_types": "computed assay",
+    #             "valid_data_dir": f"{os.path.join(TD_PATH, 'comp_valid')}",
+    #             "train_batch_size": 4,
+    #             "shuffle_buffer_size": 4,
+    #             "max_steps": 300,
+    #             "eval_steps": 2000,
+    #             "save_steps": 2000,
+    #             "dataloader_num_workers": 1,
+    #             "checkpoints_root_dir": os.path.join(TEST_DIR, "checkpoints"),
+    #             "experiment_name": "fsdp_model_train",
+    #             "gradient_accumulation_steps": 1,
+    #             "no_track": "",
+    #             "flash_attn": "",
+    #         },
+    #     )
 
-        print(f"Running command: {command}")
-        out = subprocess.run(command, shell=True, capture_output=False)
-        if out.returncode != 0:
-            raise Exception(out.stderr.decode())
+    #     print(f"Running command: {command}")
+    #     out = subprocess.run(command, shell=True, capture_output=False)
+    #     if out.returncode != 0:
+    #         raise Exception(out.stderr.decode())
 
     def test_model_valid(self):
         # clean up
